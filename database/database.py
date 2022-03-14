6067b051d5cc1b26cd12aa537ad0a20b7cf84fd5
Gabriel Corrêa <gabrielqaw@gmail.com>
2022-03-14 07:50:48 -0300

    feature: add filter for hot posts

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from pages.posts.Post import Post
 
 posts = [
-    Post('First post', 'This is the first post'),
-    Post('Second post', 'This is the second post'),
-    Post('Third post', 'This is the third post')
+    Post('First post', 'This is the first post', False),
+    Post('Second post', 'This is the second post', True),
+    Post('Third post', 'This is the third post', False),
 ]
--- pages/posts/Post.py
+++ pages/posts/Post.py
@@ -1,7 +1,8 @@
 from nanoid import generate
 
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, title, content, isHot):
         self.id = generate()
         self.title = title
         self.content = content
+        self.isHot = isHot
