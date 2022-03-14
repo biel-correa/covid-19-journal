2f066e3494ed2baca9e6715bfbc2d3b2ad1b5d12
Gabriel Corrêa <gabrielqaw@gmail.com>
2022-03-14 07:26:00 -0300

    feature: create database

---
+++ database/database.py
@@ -0,0 +1,7 @@
+from pages.posts.Post import Post
+
+posts = [
+    Post('First post', 'This is the first post'),
+    Post('Second post', 'This is the second post'),
+    Post('Third post', 'This is the third post')
+]
