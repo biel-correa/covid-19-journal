9cea20aa114850cccd945d1ab50fdb6da2bca48d
Gabriel Corrêa <gabrielqaw@gmail.com>
2022-03-28 19:43:56 -0300

    feature: add post image

--- database/database.py
+++ database/database.py
@@ -2,8 +2,8 @@ from pages.posts.Post import Post
 
 posts = [
     Post('First post', 'This is the first post', False, 'AC'),
-    Post('Second post', 'This is the second post', True, 'AC'),
-    Post('Third post', 'This is the third post', False, 'AL'),
+    Post('Second post', 'This is the second post', True, 'AC', 'https://picsum.photos/400/300'),
+    Post('Third post', 'This is the third post', False, 'AL', 'https://picsum.photos/400/300'),
 ]
 
 states = {
--- pages/posts/Post.py
+++ pages/posts/Post.py
@@ -1,9 +1,10 @@
 from nanoid import generate
 
 class Post:
-    def __init__(self, title, content, isHot, state):
+    def __init__(self, title, content, isHot, state, imageUrl=None):
         self.id = generate()
         self.title = title
         self.content = content
         self.isHot = isHot
         self.state = state
+        self.imageUrl = imageUrl
